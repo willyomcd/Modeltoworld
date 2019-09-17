c2ed66e12235c2d9083f10caae2310759003bc6f
will <willyomcd@gmail.com>
2019-09-16 23:29:31 -0600

    transforms done

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+import org.apache.commons.math3.geometry.euclidean.threed.RotationConvention;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
@@ -9,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Model {
 
@@ -25,7 +29,7 @@ public class Model {
         RealMatrix matrix = MatrixUtils.createRealMatrix(verticeListForLibrary);
        // System.out.println(matrix.toString());
         matrix = matrix.transpose();
-        //System.out.println(matrix.toString());
+        System.out.println("start" + matrix.toString());
         matrix = performManipulations(driver,matrix);
         outputFiles();
 
@@ -78,10 +82,10 @@ public class Model {
         return returnList;
     }
     private RealMatrix performManipulations(Driver d, RealMatrix matrix){
-        //matrix = rotate(d,matrix);
-        //matrix = scale(d, matrix);
-        //System.out.println(matrix.toString());
+        matrix = rotate(d,matrix);
+        matrix = scale(d, matrix);
         matrix = translate(d,matrix);
+        System.out.println("After everything" + matrix.toString());
 
         return  matrix;
     }
@@ -115,10 +119,72 @@ public class Model {
         return matrix;
     }
     private RealMatrix rotate (Driver d, RealMatrix matrix){
+        double[] axis = d.getAxis();
+        Vector3D wVector = new Vector3D(axis);
+        wVector = wVector.normalize();
+        System.out.println("normal axis: " + wVector.toString());
+        Vector3D mVector = createMVector(wVector);
+        System.out.println("mvector: " + mVector.toString());
+        Vector3D uVector = mVector.crossProduct(wVector);
+        uVector = uVector.normalize();
+        System.out.println("uvector: " + uVector);
+        Vector3D vVector = wVector.crossProduct(uVector);
+        System.out.println("vVector: " + vVector);
+
+        double [][] rotation = new double[4][4];
+        double [] homoRow = new double[]{0,0,0,1};
+        double [] uRow = new double[]{uVector.getX(),uVector.getY(),uVector.getZ(),0.0};
+        double [] vRow = new double[]{vVector.getX(),vVector.getY(),vVector.getZ(),0.0};
+        double [] wRow = new double[]{wVector.getX(),wVector.getY(),wVector.getZ(),0.0};
+
+        rotation[0] = uRow;
+        rotation[1] = vRow;
+        rotation[2] = wRow;
+        rotation[3] = homoRow;
+
+        RealMatrix rotationMatrix = MatrixUtils.createRealMatrix(rotation);
+        System.out.println("R: " + rotationMatrix.toString());
+
+        double theta = d.getAngle();
+        double thetaRadians = Math.toRadians(theta);
+        double sinTheta =  Math.sin(thetaRadians);
+        double cosTheta = Math.cos(thetaRadians);
+
+        System.out.println("sin: " + sinTheta);
+        System.out.println("cos: " + cosTheta);
+
+        double [][] rotateAboutZ = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+        rotateAboutZ[0][0] = cosTheta;
+        rotateAboutZ[1][1] = cosTheta;
+        rotateAboutZ[0][1] = -sinTheta;
+        rotateAboutZ[1][0] = sinTheta;
+        rotateAboutZ[2][2] = 1;
+        rotateAboutZ[3][3] = 1;
+        RealMatrix rotateAroundZ = MatrixUtils.createRealMatrix(rotateAboutZ);
+        System.out.println(rotateAroundZ.toString());
+
+        RealMatrix fullTransform = rotationMatrix.transpose().multiply(rotateAroundZ);
+        fullTransform= fullTransform.multiply(rotationMatrix);
+        System.out.println(fullTransform);
+
+        matrix = fullTransform.multiply(matrix);
+        System.out.println("after rotation: " + matrix.toString());
+
         return matrix;
     }
     private void outputFiles() {
 
+    }
+    private Vector3D createMVector(Vector3D normalAxis) {
+        double min = normalAxis.getX();
+        int place = 0;
+        if(normalAxis.getY() <= min){min = normalAxis.getY(); place = 1;}
+        if(normalAxis.getZ() <= min){min = normalAxis.getZ(); place = 2;}
+        double[] mAxisArray = new double[] {normalAxis.getX(),normalAxis.getY(),normalAxis.getZ()};
+        mAxisArray[place] = 1.0;
+        Vector3D mAxis= new Vector3D(mAxisArray);
+
+        return mAxis;
     }
 
 
